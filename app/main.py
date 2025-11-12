fe5aa862f41521704747704fdb441a3586610fce
Lisa <mclisa00@gmail.com>
2025-11-12 18:52:07 +0000

    Adding global exception handler to redirect to login on 401

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
 
 from .routes import auth, home
 
 app = FastAPI(title="ElbieFit")
+
+
+# TODO: this should probably go somewhere else. eventually.
+@app.exception_handler(HTTPException)
+async def auth_exception_handler(request: Request, exc: HTTPException):
+    if exc.status_code == 401:
+        return RedirectResponse(url="/auth/login")
+    raise exc
+
 
 app.include_router(home.router)
 app.include_router(auth.router)
