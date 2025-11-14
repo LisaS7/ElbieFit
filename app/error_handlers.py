51c04bcc64fc447ade462204127361b4712d5f2e
Lisa <mclisa00@gmail.com>
2025-11-14 13:32:07 +0000

    Refactored error handlers out of main.py

---
+++ app/error_handlers.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
+
+from app.templates.templates import templates
+from app.utils.log import logger
+
+
+async def http_exception_handler(request: Request, exc: HTTPException):
+    if exc.status_code == 401:
+        return RedirectResponse(url="/auth/login")
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "request": request,
+            "status_code": exc.status_code,
+            "message": exc.detail,
+        },
+        status_code=exc.status_code,
+    )
+
+
+async def unhandled_exception_handler(request: Request, exc: Exception):
+    logger.exception("Unhandled exception")
+
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "request": request,
+            "status_code": 500,
+            "message": "Gremlins.",
+        },
+        status_code=500,
+    )
+
+
+def register_error_handlers(app: FastAPI) -> None:
+    # ignore type check as function is expecting Exception type but we're giving it the more specific HTTPException
+    # which is fine, but VS Code is panicking.
+    app.add_exception_handler(HTTPException, http_exception_handler)  # type: ignore[arg-type]
+    app.add_exception_handler(Exception, unhandled_exception_handler)
--- app/main.py
+++ app/main.py
@@ -1,46 +1,11 @@
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI
 
-from app.templates.templates import templates
-from app.utils.log import logger
-
+from .error_handlers import register_error_handlers
 from .routes import auth, home, profile
 
 app = FastAPI(title="ElbieFit")
 
-
-# TODO: this should probably go somewhere else. eventually.
-@app.exception_handler(HTTPException)
-async def auth_exception_handler(request: Request, exc: HTTPException):
-    if exc.status_code == 401:
-        return RedirectResponse(url="/auth/login")
-    return templates.TemplateResponse(
-        request,
-        "error.html",
-        {
-            "request": request,
-            "status_code": exc.status_code,
-            "message": exc.detail,
-        },
-        status_code=exc.status_code,
-    )
-
-
-@app.exception_handler(Exception)
-async def unhandled_exception_handler(request: Request, exc: Exception):
-    logger.exception("Unhandled exception")
-
-    return templates.TemplateResponse(
-        request,
-        "error.html",
-        {
-            "request": request,
-            "status_code": 500,
-            "message": "Gremlins.",
-        },
-        status_code=500,
-    )
-
+register_error_handlers(app)
 
 app.include_router(home.router)
 app.include_router(auth.router)
