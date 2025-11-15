0cf27d07d5636b49820f963c9ae88d113dedbc67
Lisa <mclisa00@gmail.com>
2025-11-15 15:21:25 +0000

    Some idiot forgot to include the workout router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from .error_handlers import register_error_handlers
-from .routes import auth, home, profile
+from .routes import auth, home, profile, workout
 
 app = FastAPI(title="ElbieFit")
 
@@ -10,3 +10,4 @@ register_error_handlers(app)
 app.include_router(home.router)
 app.include_router(auth.router)
 app.include_router(profile.router)
+app.include_router(workout.router)
